b0d0e6e65f9ff4675eed6d56a866a41141416b1f
Abdulalshami <abdulkhaleq.alshami@edu.sait.ca>
2023-10-09 14:36:39 -0600

    Week4

---
+++ app/week4/page.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+import NewItem from '.app/new-item';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>My Week 4 Assignment</h1>
+      <ul>
+        <li><a href="/week4">Week 4</a></li>
+      </ul>
+      <NewItem />
+    </div>
+  );
+};
